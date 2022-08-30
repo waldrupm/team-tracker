1ef574017e8cfdb21775faec54808b54ac673776
Micheal Waldrup <waldrupm@gmail.com>
2022-08-30 14:35:36 -0400

    write test for team update

--- spec/features/teams/show_spec.rb
+++ spec/features/teams/show_spec.rb
@@ -19,4 +19,26 @@ describe 'the team show page' do
       expect(page).to have_http_status(:not_found)
     end
   end
+
+  feature 'User updates a team' do
+    scenario 'the team is updated and user see updated info' do
+      original_team = create(:team, pro: false)
+      new_name = "Edited TeamName"
+      new_last_year_finish = 3
+
+      visit team_path(original_team)
+
+      click_on "Update Team"
+      expect(current_path).to eq "/teams/#{original_team.id}/edit"
+      fill_in "team_name", with: new_name
+      fill_in "Last year finish", with: new_last_year_finish
+      check 'Pro Team'
+      click_on "Update Team"
+
+      expect(current_path).to eq "/teams/#{origina_team.id}"
+      expect(page).to have_content(new_name)
+      expect(page).to have_content(new_last_year_finish)
+      expect(page).to have_content("Yes")
+    end
+  end
 end
