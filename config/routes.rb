cb858998f3d0616b44c418d43dace3761cb38bed
Micheal Waldrup <waldrupm@gmail.com>
2022-09-01 17:49:56 -0400

    test and implement player add to team, story 13

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,9 @@ Rails.application.routes.draw do
   get '/teams/:id', to: 'teams#show', as: 'team'
   get '/teams/:id/edit', to: 'teams#edit', as: 'edit_team'
   patch '/teams/:id', to: 'teams#update'
+  get '/teams/:team_id/players/new', to: 'players#new', as: 'new_team_player'
   get '/teams/:id/players', to: 'teams#player_list', as: 'team_player_list'
+  post '/teams/:team_id/players', to: 'players#create', as: 'create_team_player'
 
   get '/players', to: 'players#index'
   get '/players/:id', to: 'players#show', as: 'player'
